c38182d5b3ea2a3cef5ca63d5359e4a63580a7f1
ANTOINE_CASSAGNE <cassagnea@gmail.com>
2023-05-11 15:54:42 +0200

    map creation

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState, useRef } from "react";
+import L from "leaflet";
+import { TileLayer, MapContainer, LayersControl } from "react-leaflet";
+import { Button } from "@material-ui/core";
+
+import RoutingControl from "./RoutingControl";
+
+const maps = {
+  base: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+};
+
+const Map = () => {
+  const [map, setMap] = useState(null);
+  const [start, setStart] = useState([38.9072, -77.0369]);
+  const [end, setEnd] = useState([37.7749, -122.4194]);
+
+  return (
+    <>
+      <MapContainer
+        center={[37.0902, -95.7129]}
+        zoom={3}
+        zoomControl={false}
+        style={{ height: "100vh", width: "100%", padding: 0 }}
+        whenCreated={(map) => setMap(map)}>
+        {/* *************** */}
+        {/* Pass in our custom control layer here, inside of the map container */}
+        {/* *************** */}
+        <RoutingControl
+          position={"topleft"}
+          start={start}
+          end={end}
+          color={"#757de8"}
+        />
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="Map">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url={maps.base}
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
+      </MapContainer>
+    </>
+  );
+};
+
+export default Map;
