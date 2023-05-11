284877bdc84dc4664e04c1d34e44b9320f10d325
Charlie FEIX <contact@charliefeix.fr>
2023-05-11 22:24:55 +0200

    Nav bar implement

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,53 +6,35 @@ import "./Map.scss";
 import RoutingControl from "./RoutingControl";
 
 const maps = {
-  base: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    base: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
 };
 
 const Map = ({ departureResult, destinationResult }) => {
-  const [map, setMap] = useState(null);
-  const [start, setStart] = useState(departureResult.coordinates);
-  const [end, setEnd] = useState(destinationResult.coordinates);
-  const [distance, setDistance] = useState(null);
+    const [map, setMap] = useState(null);
+    const [distance, setDistance] = useState(null);
 
-  console.log(departureResult);
-  console.log(destinationResult);
+    const handleRoutesFound = (e) => {
+        const route = e.routes[0];
+        const distanceInKm = route.summary.totalDistance / 1000;
+        setDistance(distanceInKm);
+    };
 
-  const handleRoutesFound = (e) => {
-    const route = e.routes[0];
-    const distanceInKm = route.summary.totalDistance / 1000;
-    setDistance(distanceInKm);
-  };
-  // console.log(distanceInKm);
-  return (
-    <>
-      <div className="mapContent">
-        <MapContainer
-          center={[37.0902, -95.7129]}
-          zoomLevel={5}
-          zoomControl={false}
-          style={{ height: "50vh", width: "100%", padding: 0 }}
-          whenCreated={(map) => setMap(map)}>
-          <RoutingControl
-            position={"topleft"}
-            start={start}
-            end={end}
-            color={"#757de8"}
-            onRoutesFound={handleRoutesFound}
-          />
-          <LayersControl position="topright">
-            <LayersControl.BaseLayer checked name="Map">
-              <TileLayer
-                attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>'
-                url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
-              />
-            </LayersControl.BaseLayer>
-          </LayersControl>
-        </MapContainer>
-        <div>{distance && <p>Distance: {distance} km</p>}</div>
-      </div>
-    </>
-  );
+    // console.log(distanceInKm);
+    return (
+        <>
+            <div className="mapContent">
+                <MapContainer center={[37.0902, -95.7129]} zoomLevel={5} zoomControl={false} style={{ height: "50vh", width: "100%", padding: 0 }} whenCreated={(map) => setMap(map)}>
+                    <RoutingControl position={"topleft"} start={departureResult.coordinates} end={destinationResult.coordinates} color={"#0d9488"} onRoutesFound={handleRoutesFound} />
+                    <LayersControl position="topright">
+                        <LayersControl.BaseLayer checked name="Map">
+                            <TileLayer attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>' url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png" />
+                        </LayersControl.BaseLayer>
+                    </LayersControl>
+                </MapContainer>
+                <div>{distance && <p>Distance: {distance} km</p>}</div>
+            </div>
+        </>
+    );
 };
 
 export default Map;
--- src/pages/Results/Results.jsx
+++ src/pages/Results/Results.jsx
@@ -3,16 +3,22 @@ import Events from "../../components/Events/Events";
 import Booking from "../../components/Booking/Booking";
 import Map from "../../components/Map/Map";
 import Weather from "../../components/Weather/Weather";
+import NavBar from "../../components/NavBar/NavBar";
+import "./Results.scss";
 
 function Results({ setDepartureResult, setDestinationResult, departureResult, destinationResult }) {
-  return (
-    <div>
-      <Booking />
-      <Events />
-      <Map departureResult={departureResult} destinationResult={destinationResult} />
-      <Weather />
-    </div>
-  );
+    return (
+        <div id="results">
+            <NavBar setDepartureResult={setDepartureResult} setDestinationResult={setDestinationResult} departureResult={departureResult} destinationResult={destinationResult} />
+
+            <div className="container">
+                <Booking departureResult={departureResult} destinationResult={destinationResult} />
+                <Events departureResult={departureResult} destinationResult={destinationResult} />
+                <Map departureResult={departureResult} destinationResult={destinationResult} />
+                <Weather />
+            </div>
+        </div>
+    );
 }
 
 export default Results;
