6d6e89873f4db1b5fb88289d6f73141947ae46ac
Charlie FEIX <contact@charliefeix.fr>
2023-05-12 12:47:16 +0200

    Logo added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,19 @@
 import "./NavBar.scss";
 import SearchInputs from "../SearchInputs/SearchInputs";
+import { Link } from "react-router-dom";
 
 export default function NavBar({ setDepartureResult, setDestinationResult, departureResult, destinationResult }) {
     return (
         <nav className="nav-bar">
-            <SearchInputs  departureResult={departureResult} destinationResult={destinationResult} setDepartureResult={setDepartureResult} setDestinationResult={setDestinationResult} />
+            <div className="nav-container">
+                <Link to={`/`} className="logo-container">
+                    <div className="logo">
+                        <img src="/assets/logo.svg" alt="Touriosity logo" />
+                    </div>
+                    <p>Touriosity</p>
+                </Link>
+                <SearchInputs departureResult={departureResult} destinationResult={destinationResult} setDepartureResult={setDepartureResult} setDestinationResult={setDestinationResult} />
+            </div>
         </nav>
     );
 }
